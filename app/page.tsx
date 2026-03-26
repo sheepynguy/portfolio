356ca8cdfc900507b71877faf975b6ed96fa1130
sheepynguy <vitonguy6@gmail.com>
2026-03-25 22:19:23 -0500

    Added base containers and easter egg button

--- app/page.tsx
+++ app/page.tsx
@@ -1,65 +1,103 @@
+"use client";
 import Image from "next/image";
+import useSound from "use-sound";
 
 export default function Home() {
+  const [easteregg, { sound }] = useSound('/soundeffects/witnesstestimony.mp3', {volume: 0.25});
+
+  const handleEasterEggButton = () => {
+    easteregg();
+    setTimeout(() => {
+      sound?.stop();
+    }, 1000);
+  }
+
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
+    <div className="min-h-screen">
+      {/* main body container */}
+      <div className="grid justify-center gap-4">
+        {/* top and bottom container */}
+        {/* this lets the rows size to their content */}
+        <div className="grid grid-cols-2 gap-4">
+          {/* top container */}
+          <div className="bg-green-700">
+            {/* left container */}
+            {/* This will later contain the bunny animation */}
+          </div>
+
+          <div className="flex justify-center bg-green-300"> 
+            {/* right container */}
+            <div className="flex flex-col">
+            {/* entire text container */}
+              <div className="flex-col">
+                <p>
+                  My name is Victoria Nguyen.
+                </p>
+                <p>
+                  {/* maybe I can add some special styling for this */}
+                  I'm a fullstack developer.
+                </p>
+              </div>
+            </div>
+          </div>
         </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
+
+        <div className="grid gap-2 max-w-[1000px] mx-auto px-4">
+          {/* bottom container */}
+          <div className="flex">
+            {/* container */}
+            I'm a fullstack engineer focused on building simple but effective web tools.
+            I enjoy working on designing and creating a user experience that is built on clean and scalable code.
+          </div>
+
+          <div className="">
+            {/* container */}
+            <p>
+              I'm currently a full stack developer at{" "}
+            <a className="transition duration-500 ease-in-out hover:text-green-500 underline" href="https://www.dssedu.com/">
+              Dynamic Support Solutions
+            </a>
+              , where I work on their financial tool that brings school financials into a much easier viewing experience for all user groups.
+              In this role, I work on extradordinary items while parterning with other developers to identify bugs and implement fixes to the main pages.
+            </p>
+          </div>
+                    
+          <div className="flex flex-row">
+            {/* container */}
+            {/* may or may not need this container */}
+            <p>
+              Aside from my work, I've also done personal projects such as a{" "}
+            <a>
+              text-based game
+            </a>
+            , a{" "}
+            <a>
+              task tracker tool
+            </a>
+            , and I'm currently working on a{" "}
+            <a>
+              2D-pixel adventure game
+            </a>
+              . These experiences have shaped how I approach building products that are clean and usable. 
+            </p>
+          </div>
+                    
+          <div className="">
+            {/* container */}
+            <p>
+              In my spare time, you can either catch me watching anime, hanging out with friends, or having a{" "}
+
+            <button 
+              className="transition-all duration-500 hover:bg-gradient-to-r hover:from-blue-500 hover:to-red-500 hover:bg-clip-text hover:text-transparent" 
+              onClick={handleEasterEggButton}
+            >
+              turnabout in the courtroom
+            </button>
+              .
+            </p>
+          </div>
         </div>
-      </main>
+      </div>
     </div>
   );
 }
